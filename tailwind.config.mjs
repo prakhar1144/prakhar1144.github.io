698c0071b521a8b943c0f219b442c1da1dce8b3d
Prakhar Pratyush <prakhar1144@gmail.com>
2025-12-25 14:59:52 +0530

    feat: Public release.
    Signed-off-by: Prakhar Pratyush <prakhar1144@gmail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			listStyleType: {
+				circle: 'circle',
+			},
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography'),],
 }
